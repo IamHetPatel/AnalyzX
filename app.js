0c96c76a9237a8a31fbcc613c15b966b01703838
dharmikjethva30 <dharmikjethva4@gmail.com>
2023-03-26 09:21:24 +0530

    cors fixed

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./model/user');
+const cors = require("cors")
+
 
 
 const app = express();
+app.use(cors())
 app.use(express.json())
 
 const connectDB = async () => {
